ec18918580014e215351e2f5ecdeda217833e04f
Dave Fletcher <davemfletch@gmail.com>
2017-06-20 12:08:03 +1200

    Problem15 created

---
+++ ProjectEuler/csharp/Problem15.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.csharp
+{
+    public class Problem15 : IProblem
+    {
+
+        public struct ValueTuple<T1, T2>
+        {
+
+        }
+
+
+
+
+        public Answer GetAnswer()
+        {
+            var tuple1 = (1, 2);
+            Console.WriteLine(tuple1.Item1);
+            Console.WriteLine(tuple1.Item2);
+
+            return new Answer
+            {
+
+            };
+        }
+    }
+}
