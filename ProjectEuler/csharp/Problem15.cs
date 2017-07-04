f85207a6ca91cfe0694910a635361cbe32c58e34
Dave Fletcher <davemfletch@gmail.com>
2017-07-04 13:57:10 +1200

    second attempt method

--- ProjectEuler/csharp/Problem15.cs
+++ ProjectEuler/csharp/Problem15.cs
@@ -10,6 +10,8 @@ namespace ProjectEuler
     /// Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
 
     //  How many such routes are there through a 20×20 grid?
+
+    //  Answer = 137846528820
     /// </summary>
     public class Problem15 : IProblem
     {
@@ -43,5 +45,19 @@ namespace ProjectEuler
             }
             return grid[0, 0];
         }
+
+
+        private double SecondAttempt(int gridSize = Grid)
+        {
+            long paths = 1;
+            for(int i=0; i< gridSize; i++)
+            {
+                paths *= (2 * gridSize) - i;
+                paths /= i + 1;
+            }
+            return paths;
+
+        }
+        
     }
 }
